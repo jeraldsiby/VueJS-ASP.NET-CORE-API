c7b11c9fc19be09858c5e214fef8000db657c11b
Jerald Siby <jeraldsiby@Jeralds-MacBook-Air.local>
2020-05-26 16:19:37 -0400

    added bootstrap to the vue project

--- Vue/first-vue-app/src/main.js
+++ Vue/first-vue-app/src/main.js
@@ -1,15 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
 
-Vue.config.productionTip = false
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+import router from "./router";
+
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
-  template: '<App/>',
+  template: "<App/>",
   components: { App }
-})
+});
