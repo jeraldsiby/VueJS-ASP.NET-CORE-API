a299be1acfc51795cfc7f7723d3c8aa7f27ba72f
Jerald <jeraldsiby05@gmail.com>
2020-05-26 19:47:17 -0400

    authentication component added (okta)

--- Vue/first-vue-app/src/router/index.js
+++ Vue/first-vue-app/src/router/index.js
@@ -1,15 +1,37 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
+// Vue imports
+import Vue from "vue";
+import Router from "vue-router";
 
-Vue.use(Router)
+// 3rd party imports
+import Auth from "@okta/okta-vue";
 
-export default new Router({
+// our own imports
+import Hello from "@/components/Hello";
+
+Vue.use(Auth, {
+  issuer: "https://{dev-715224-admin.okta.com}/oauth2/default",
+  client_id: "{0oad5t3ezzmOlWupK4x6}",
+  redirect_uri: "http://localhost:8080/implicit/callback",
+  scope: "openid profile email"
+});
+
+Vue.use(Router);
+
+let router = new Router({
+  mode: "history",
   routes: [
     {
-      path: '/',
-      name: 'Hello',
+      path: "/",
+      name: "Hello",
       component: Hello
+    },
+    {
+      path: "/implicit/callback",
+      component: Auth.handleCallback()
     }
   ]
-})
+});
+
+router.beforeEach(Vue.prototype.$auth.authRedirectGuard());
+
+export default router;
