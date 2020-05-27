b94a409f81adb4c2eb17525e2afc664e51d3d6f6
Jerald <jeraldsiby05@gmail.com>
2020-05-26 22:58:56 -0400

    added the nuget package -Microsoft.AspNetCore.Authentication.JwtBearer

--- AspNetCore/Startup.cs
+++ AspNetCore/Startup.cs
@@ -40,6 +40,7 @@ namespace AspNetCore
                                         options.Authority = Configuration["Okta:Authority"];
                                         options.Audience = "api://default";
                                     });
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
